c14031d636e52d7b8bfa70ecafed0629b038be2b
mhmdnab <mhmdnab004@gmail.com>
2025-06-02 23:34:18 +0300

    backend refining 24

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -6,12 +6,12 @@ dotenv.config();
 const SUPABASE_URL = process.env.SUPABASE_URL!;
 const SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-if (!SUPABASE_URL) {
-  throw new Error("Missing supabase url!!");
-}
-
 if (!SERVICE_ROLE_KEY) {
   throw new Error("Missing supabase role key!!");
+}
+
+if (!SUPABASE_URL) {
+  throw new Error("Missing supabase url!!");
 }
 
 // Remove the unsupported `fetch` field from the options object:
